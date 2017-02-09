3fbc545cac581c59b69168a844d260bec6c39ce5
Sporious <Mephisto144@hotmail.com>
2017-02-09 13:04:11 +0000

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,67 @@
+var computerGuess
+function compResult()
+{
+  var guess = Math.random();
+  if (guess < 1/3) computerGuess = "Rock";
+  if (guess < 2/3) computerGuess = "Scissors";
+  if (guess <= 1 ) computerGuess = "Paper";
+  alert("computerGuess " + computerGuess)
+}
+
+function answer( input )
+{
+  alert("input = " + input)
+  if ( input == "Rock" )
+  {
+    switch (computerGuess)
+    {
+      case "Rock":
+        alert("Draw");
+        break;
+      case "Scissors":
+        alert("Win");
+        break;
+      case "Paper":
+        alert("Lose");
+        break;
+      default:
+        alert("error");
+      }
+    }
+    if ( input == "Paper" )
+    {
+      switch (computerGuess)
+      {
+        case "Rock":
+          alert("Win");
+          break;
+        case "Scissors":
+          alert("Lose");
+          break;
+        case "Paper":
+          alert("Draw");
+          break;
+        default:
+          alert("error");
+        }
+      }
+      if ( input == "Scissors" )
+      {
+        switch (computerGuess)
+        {
+          case "Rock":
+            alert("Lose");
+            break;
+          case "Scissors":
+            alert("Draw");
+            break;
+          case "Paper":
+            alert("Win");
+            break;
+          default:
+            alert("error");
+          }
+        }
+        compResult();
+}
+compResult();
